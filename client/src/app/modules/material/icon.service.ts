41b3653ca42d63383343d8b0d2c98239da5c7be9
Sanjeev Premi <spremi@ymail.com>
2019-03-16 23:06:18 +0530

    chore: c: mod: material: Add IconService
    Command:
      ng g s modules/material/icon --lintFix=true
    
    Also added file headers.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/modules/material/icon.service.ts
@@ -0,0 +1,17 @@
+//
+// [build-smith-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconService {
+
+  constructor() { }
+}
