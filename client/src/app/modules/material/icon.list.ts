b2c124286df02ec30d5c67aef2f2b59fe91bac61
Sanjeev Premi <spremi@ymail.com>
2019-03-16 23:04:26 +0530

    feat: c: mod: material: List initial set of icons
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/modules/material/icon.list.ts
@@ -0,0 +1,259 @@
+//
+// [build-smith-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+import { IconSet } from './icon-set';
+
+/**
+ * Application logo.
+ */
+export const Logo: IconSet = {
+  path: '/assets/logo/',
+  list: [
+    {
+      icon: 'logo_color',
+      file: 'logo-color.svg',
+    },
+    {
+      icon: 'logo',
+      file: 'logo-regular.svg',
+    },
+    {
+      icon: 'logo_inverse',
+      file: 'logo-inverse.svg',
+    },
+  ],
+};
+
+/**
+ * List of common icons.
+ */
+export const AppIcons: IconSet = {
+  path: '/assets/icons/',
+  list: [
+    {
+      icon: 'add',
+      file: 'ic_add_24px.svg',
+    },
+    {
+      icon: 'arrow_back',
+      file: 'ic_arrow_back_24px.svg',
+    },
+    {
+      icon: 'arrow_downward',
+      file: 'ic_arrow_downward_24px.svg',
+    },
+    {
+      icon: 'arrow_forward',
+      file: 'ic_arrow_forward_24px.svg',
+    },
+    {
+      icon: 'arrow_upward',
+      file: 'ic_arrow_upward_24px.svg',
+    },
+    {
+      icon: 'block',
+      file: 'ic_block_24px.svg',
+    },
+    {
+      icon: 'check',
+      file: 'ic_check_24px.svg',
+    },
+    {
+      icon: 'clear',
+      file: 'ic_clear_24px.svg',
+    },
+    {
+      icon: 'create',
+      file: 'ic_create_24px.svg',
+    },
+    {
+      icon: 'dashboard',
+      file: 'ic_dashboard_24px.svg',
+    },
+    {
+      icon: 'delete',
+      file: 'ic_delete_24px.svg',
+    },
+    {
+      icon: 'error_outline',
+      file: 'ic_error_outline_24px.svg',
+    },
+    {
+      icon: 'favorite',
+      file: 'ic_favorite_24px.svg',
+    },
+    {
+      icon: 'gavel',
+      file: 'ic_gavel_24px.svg',
+    },
+    {
+      icon: 'help_outline',
+      file: 'ic_help_outline_24px.svg',
+    },
+    {
+      icon: 'history',
+      file: 'ic_history_24px.svg',
+    },
+    {
+      icon: 'home',
+      file: 'ic_home_24px.svg',
+    },
+    {
+      icon: 'hourglass',
+      file: 'ic_hourglass_empty_24px.svg',
+    },
+    {
+      icon: 'info_outline',
+      file: 'ic_info_outline_24px.svg',
+    },
+    {
+      icon: 'list',
+      file: 'ic_list_24px.svg',
+    },
+    {
+      icon: 'lock_open',
+      file: 'ic_lock_open_24px.svg',
+    },
+    {
+      icon: 'lock_outline',
+      file: 'ic_lock_outline_24px.svg',
+    },
+    {
+      icon: 'more_vert',
+      file: 'ic_more_vert_24px.svg',
+    },
+    {
+      icon: 'person',
+      file: 'ic_person_24px.svg',
+    },
+    {
+      icon: 'save',
+      file: 'ic_save_24px.svg',
+    },
+    {
+      icon: 'search',
+      file: 'ic_search_24px.svg',
+    },
+    {
+      icon: 'settings',
+      file: 'ic_settings_24px.svg',
+    },
+    {
+      icon: 'tick',
+      file: 'ic_settings_24px.svg',
+    },
+    {
+      icon: 'verified_user',
+      file: 'ic_verified_user_24px.svg',
+    },
+  ],
+};
+
+/**
+ * List of status icons.
+ */
+export const StatusIcons: IconSet = {
+  path: '/assets/icons/status/',
+  list: [
+    {
+      icon: 'status_abort',
+      file: 'status-abort.svg',
+    },
+    {
+      icon: 'status_disabled',
+      file: 'status-disabled.svg',
+    },
+    {
+      icon: 'status_failure',
+      file: 'status-failure.svg',
+    },
+    {
+      icon: 'status_never',
+      file: 'status-never.svg',
+    },
+    {
+      icon: 'status_ongoing',
+      file: 'status-ongoing.svg',
+    },
+    {
+      icon: 'status_success',
+      file: 'status-success.svg',
+    },
+  ],
+};
+
+/**
+ * List of health icons.
+ */
+export const HealthIcons: IconSet = {
+  path: '/assets/icons/health/',
+  list: [
+    {
+      icon: 'health_l0',
+      file: 'health-l0.svg',
+    },
+    {
+      icon: 'health_l1',
+      file: 'health-l1.svg',
+    },
+    {
+      icon: 'health_l2',
+      file: 'health-l2.svg',
+    },
+    {
+      icon: 'health_l3',
+      file: 'health-l3.svg',
+    },
+    {
+      icon: 'health_l4',
+      file: 'health-l4.svg',
+    },
+    {
+      icon: 'health_l5',
+      file: 'health-l5.svg',
+    },
+  ],
+};
+
+/**
+ * List of access icons.
+ */
+export const AccessIcons: IconSet = {
+  path: '/assets/icons/access/',
+  list: [
+    {
+      icon: 'access_allow',
+      file: 'access-allow.svg',
+    },
+    {
+      icon: 'access_block',
+      file: 'access-block.svg',
+    },
+    {
+      icon: 'access_unknown',
+      file: 'access-unknown.svg',
+    },
+  ],
+};
+
+/**
+ * List of state icons.
+ */
+export const StateIcons: IconSet = {
+  path: '/assets/icons/state/',
+  list: [
+    {
+      icon: 'state_enabled',
+      file: 'state-enabled.svg',
+    },
+    {
+      icon: 'state_disabled',
+      file: 'state-disabled.svg',
+    },
+  ],
+};
