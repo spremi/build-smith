8f5027a5ab7970720fe7e9a23da0a7ef065c3c21
Sanjeev Premi <spremi@ymail.com>
2019-03-16 23:13:55 +0530

    feat: c: mod: material: icon-svc: Implement register()
    Registers known icons into icon registry.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/modules/material/icon.service.ts
+++ client/src/app/modules/material/icon.service.ts
@@ -7,11 +7,46 @@
 //
 
 import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { IconInfo, IconSet } from './icon-set';
+import {
+  AccessIcons, AppIcons, HealthIcons, Logo, StateIcons, StatusIcons
+} from './icon.list';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IconService {
+  constructor(private registry: MatIconRegistry, private sanitizer: DomSanitizer) { }
 
-  constructor() { }
+  /**
+   * Register all icons.
+   */
+  register() {
+    this.registerSet(Logo);
+    this.registerSet(AppIcons);
+    this.registerSet(AccessIcons);
+    this.registerSet(StatusIcons);
+    this.registerSet(StateIcons);
+    this.registerSet(HealthIcons);
+  }
+
+  /**
+   * Register icons in specified set.
+   */
+  private registerSet(set: IconSet) {
+    if (set.list.length === 0) {
+      return;
+    }
+
+    set.list.forEach((arg: IconInfo) => {
+      const iconPath = (set.path + '/' + arg.file).replace('//', '/');
+
+      this.registry.addSvgIcon(
+        arg.icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(iconPath));
+    });
+  }
 }
