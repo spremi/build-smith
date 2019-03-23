90b47980a18095d699dc8ccdaf34a10935cbffba
Sanjeev Premi <spremi@ymail.com>
2019-03-24 00:14:56 +0530

    feat: c: models: Add generic interfaces
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/models/generic.ts
@@ -0,0 +1,18 @@
+//
+// [build-smith-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+/**
+ * Pair of Label & Value mostly used with drop-down lists.
+ */
+export interface LabelValue {
+  /** Label displayed. */
+  label: string;
+
+  /** Actual value. */
+  value: string;
+}
