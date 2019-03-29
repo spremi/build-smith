ae0dfa49b48a97b3269ba529f5cd339210a9651e
Sanjeev Premi <spremi@ymail.com>
2019-03-30 01:28:21 +0530

    test: c: app: Fix failing test cases
    Remove test cases that are no longer valid.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -10,6 +10,26 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sp-header',
+  template: '<span>Mock Header</span>',
+})
+export class MockHeaderComponent { }
+
+@Component({
+  selector: 'sp-body',
+  template: '<span>Mock Body</span>',
+})
+export class MockBodyComponent { }
+
+@Component({
+  selector: 'sp-footer',
+  template: '<span>Mock Footer</span>',
+})
+export class MockFooterComponent { }
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,26 +38,17 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        MockHeaderComponent,
+        MockBodyComponent,
+        MockFooterComponent,
       ],
-    }).compileComponents();
+    })
+      .compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'build-smith-client'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('build-smith-client');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to build-smith-client!');
   });
 });
