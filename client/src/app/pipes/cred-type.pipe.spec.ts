625e82945d88cdd640863c06d362618edbd9d5b7
Sanjeev Premi <spremi@ymail.com>
2019-04-23 12:03:25 +0530

    feat: c: pipes/cred-type: Initial implementation
    ...including unit test cases.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/pipes/cred-type.pipe.spec.ts
+++ client/src/app/pipes/cred-type.pipe.spec.ts
@@ -6,11 +6,25 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
+import { CredentialType } from '../models/credentials';
 import { CredTypePipe } from './cred-type.pipe';
 
 describe('CredTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CredTypePipe();
-    expect(pipe).toBeTruthy();
+  let pipe: CredTypePipe;
+
+  beforeEach(() => {
+    pipe = new CredTypePipe();
+  });
+
+  it('transforms CredentialType.USER_PASS', () => {
+    expect(pipe.transform(CredentialType.USER_PASS)).toBe('USER + PASS');
+  });
+
+  it('transforms CredentialType.USER_PASS', () => {
+    expect(pipe.transform(CredentialType.SSH_PKEY)).toBe('SSH + PKEY');
+  });
+
+  it('transforms CredentialType.USER_PASS', () => {
+    expect(pipe.transform(CredentialType.AUTH_TOKEN)).toBe('AUTH TOKEN');
   });
 });
--- client/src/app/pipes/cred-type.pipe.ts
+++ client/src/app/pipes/cred-type.pipe.ts
@@ -7,14 +7,33 @@
 //
 
 import { Pipe, PipeTransform } from '@angular/core';
+import { CredentialType } from '../models/credentials';
 
 @Pipe({
   name: 'credType',
 })
 export class CredTypePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(value: string): string {
+    let ret: string;
 
+    switch (value) {
+      case CredentialType.USER_PASS:
+        ret = 'USER + PASS';
+        break;
+
+      case CredentialType.SSH_PKEY:
+        ret = 'SSH + PKEY';
+        break;
+
+      case CredentialType.AUTH_TOKEN:
+        ret = 'AUTH TOKEN';
+        break;
+
+      default:
+        ret = null;
+    }
+
+    return ret;
+  }
 }
