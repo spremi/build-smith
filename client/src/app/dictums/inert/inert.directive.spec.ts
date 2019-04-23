a04055b3bb253447f9d86e4faea74580e917aa03
Sanjeev Premi <spremi@ymail.com>
2019-04-23 13:33:40 +0530

    feat: c: dictums/inert: Initial implementation
    ...including unit test cases.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/dictums/inert/inert.directive.spec.ts
+++ client/src/app/dictums/inert/inert.directive.spec.ts
@@ -6,11 +6,56 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
+import { Component } from '@angular/core';
+import {
+  async, ComponentFixture, ComponentFixtureAutoDetect, TestBed
+} from '@angular/core/testing';
+
 import { InertDirective } from './inert.directive';
 
+@Component({
+  selector: 'sp-test',
+  template: `
+    <div>
+      <div id="a" [spInert]="true"></div>
+      <div id="b" [spInert]="false"></div>
+    </div>
+  `,
+})
+class TestComponent { }
+
 describe('InertDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InertDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let testComponent: TestComponent;
+  let testContainer: HTMLDivElement;
+
+  beforeEach(async (() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        InertDirective,
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+
+    testComponent = fixture.componentInstance;
+    testContainer = fixture.nativeElement;
+  }));
+
+
+  it('should have class sp-inert (when true)', () => {
+    const targetDiv = testContainer.querySelector('#a') as HTMLDivElement;
+
+    expect(targetDiv.classList.contains('sp-inert')).toBeTruthy();
+  });
+
+  it('shouldn\'t have class sp-inert (when false)', () => {
+    const targetDiv = testContainer.querySelector('#b') as HTMLDivElement;
+
+    expect(targetDiv.classList.contains('sp-inert')).toBeFalsy();
   });
 });
--- client/src/app/dictums/inert/inert.directive.ts
+++ client/src/app/dictums/inert/inert.directive.ts
@@ -6,13 +6,20 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[spInert]',
 })
 export class InertDirective {
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
-  constructor() { }
-
+  @Input('spInert')
+  set inert(flag: boolean) {
+    if (flag) {
+      this.renderer.addClass(this.el.nativeElement, 'sp-inert');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement, 'sp-inert');
+    }
+  }
 }
