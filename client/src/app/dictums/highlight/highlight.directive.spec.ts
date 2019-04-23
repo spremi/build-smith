a706709bdc0f57072a0e5d167429b3ebc98fb25b
Sanjeev Premi <spremi@ymail.com>
2019-04-23 13:29:45 +0530

    feat: c: dictums/highlight: Initial implementation
    ...including unit test cases.
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/dictums/highlight/highlight.directive.spec.ts
+++ client/src/app/dictums/highlight/highlight.directive.spec.ts
@@ -6,11 +6,58 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
+import { Component } from '@angular/core';
+import {
+  async, ComponentFixture, ComponentFixtureAutoDetect, TestBed
+} from '@angular/core/testing';
+
 import { HighlightDirective } from './highlight.directive';
 
+@Component({
+  selector: 'sp-test',
+  template: `
+  <div>
+    <div id="a" spHighlight></div>
+  </div>
+  `,
+})
+class TestComponent { }
+
 describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let testComponent: TestComponent;
+  let testContainer: HTMLDivElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        HighlightDirective,
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestComponent);
+
+    testComponent = fixture.componentInstance;
+    testContainer = fixture.nativeElement;
+  }));
+
+  it('should have elevation on "mouseenter"', () => {
+    const targetDiv = testContainer.querySelector('#a') as HTMLDivElement;
+
+    expect(targetDiv.classList.contains('mat-elevation-z2')).toBeFalsy();
+
+    targetDiv.dispatchEvent(new MouseEvent('mouseenter'));
+    fixture.detectChanges();
+
+    expect(targetDiv.classList.contains('mat-elevation-z2')).toBeTruthy();
+
+    targetDiv.dispatchEvent(new MouseEvent('mouseleave'));
+    fixture.detectChanges();
+
+    expect(targetDiv.classList.contains('mat-elevation-z2')).toBeFalsy();
   });
 });
--- client/src/app/dictums/highlight/highlight.directive.ts
+++ client/src/app/dictums/highlight/highlight.directive.ts
@@ -6,13 +6,30 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[spHighlight]',
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.highlight(true);
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.highlight(false);
+  }
+
+  private highlight(flag: boolean) {
+    if (flag) {
+      this.renderer.addClass(this.el.nativeElement, 'mat-elevation-z2');
+    } else {
+      this.renderer.removeClass(this.el.nativeElement, 'mat-elevation-z2');
+    }
+  }
 }
