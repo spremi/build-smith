963e06beb50f520c759854485428d008ae6028a0
Sanjeev Premi <spremi@ymail.com>
2019-04-23 15:53:16 +0530

    feat: c: models: Add interfaces related to build trigger
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ client/src/app/models/triggers.ts
@@ -0,0 +1,51 @@
+//
+// [build-smith-client]
+//
+// Sanjeev Premi <spremi@ymail.com>
+//
+// Available under terms of the BSD-3-Clause license.
+//
+
+/**
+ * Different triggers
+ */
+export class TriggerType {
+  /** On user action. */
+  public static readonly USER = 'U';
+
+  /** On receiving web-hook. */
+  public static readonly HOOK = 'H';
+
+  /** Periodically. */
+  public static readonly PERIODIC = 'P';
+
+  /** After other project(s). */
+  public static readonly AFTER = 'A';
+}
+
+/**
+ * Describes a trigger.
+ */
+export interface Trigger {
+  /** Type of trigger */
+  type: TriggerType;
+
+  /** Hook identifier - if trigger type is 'HOOK'. */
+  hook: string;
+
+  /** Period (in minutes) - if trigger type is 'PERIODIC'. */
+  period: number;
+
+  /** Project identifier - if trigger type is 'AFTER'. */
+  project: string;
+}
+
+/**
+ * Initializer for interface 'Trigger'.
+ */
+export const initTrigger = (): Trigger => ({
+  type: null,
+  hook: null,
+  period: null,
+  project: null,
+});
