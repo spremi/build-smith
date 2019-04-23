2c17b6fe691e3c18fec17756d5b600a39395d2eb
Sanjeev Premi <spremi@ymail.com>
2019-04-23 15:30:16 +0530

    feat: c: models/generic: Add 'Condition' for build steps
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/models/generic.ts
+++ client/src/app/models/generic.ts
@@ -16,3 +16,115 @@ export interface LabelValue {
   /** Actual value. */
   value: string;
 }
+
+/**
+ * List of numeric comparisons.
+ */
+export class NumComparison {
+  public static readonly EQ = 'n_eq';
+  public static readonly NE = 'n_ne';
+  public static readonly LT = 'n_lt';
+  public static readonly GT = 'n_gt';
+  public static readonly LE = 'n_le';
+  public static readonly GE = 'n_ge';
+}
+
+/**
+ * List of string comparisons.
+ */
+export class StrComparison {
+  public static readonly EQ = 's_equ';
+  public static readonly NE = 's_neq';
+  public static readonly STARTS = 's_stt';
+  public static readonly ENDS = 's_end';
+  public static readonly CONTAINS = 's_con';
+  public static readonly MATCHES = 's_mat';
+}
+
+/**
+ * List of numeric compare options.
+ */
+export const NumCompareOptions: LabelValue[] = [
+  {
+    label: 'Equals (Numeric)',
+    value: NumComparison.EQ,
+  },
+  {
+    label: 'Not Equals (Numeric)',
+    value: NumComparison.NE,
+  },
+  {
+    label: 'Less than',
+    value: NumComparison.LT,
+  },
+  {
+    label: 'Greated than',
+    value: NumComparison.GT,
+  },
+  {
+    label: 'Less than or equal',
+    value: NumComparison.LE,
+  },
+  {
+    label: 'Greated than or equal',
+    value: NumComparison.GE,
+  },
+];
+
+/**
+ * List of numeric compare options.
+ */
+export const StrCompareOptions: LabelValue[] = [
+  {
+    label: 'Equals (String)',
+    value: StrComparison.EQ,
+  },
+  {
+    label: 'Not Equals (String)',
+    value: StrComparison.NE,
+  },
+  {
+    label: 'Starts with',
+    value: StrComparison.STARTS,
+  },
+  {
+    label: 'Ends with',
+    value: StrComparison.ENDS,
+  },
+  {
+    label: 'Contains',
+    value: StrComparison.CONTAINS,
+  },
+  {
+    label: 'Matches',
+    value: StrComparison.MATCHES,
+  },
+];
+
+
+/**
+ * Describe a condition.
+ */
+export interface Condition {
+  /** First variable / expression. */
+  a: string;
+
+  /** Second variable /expression. */
+  b: string;
+
+  /** Type of comparison */
+  comparison: NumComparison | StrComparison;
+
+  /** Is string comparison case-sensitive? */
+  case: boolean;
+}
+
+/**
+ * Initializer for interface 'Condition'.
+ */
+export const initCondition = (): Condition => ({
+  a: null,
+  b: null,
+  comparison: StrComparison.EQ,
+  case: true,
+});
