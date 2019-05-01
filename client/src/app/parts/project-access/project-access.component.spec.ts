100e5266bfaae9329235805b5185ccd080503215
Sanjeev Premi <spremi@ymail.com>
2019-05-01 20:42:54 +0530

    feat: c: parts/project-access: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/project-access/project-access.component.spec.ts
+++ client/src/app/parts/project-access/project-access.component.spec.ts
@@ -8,6 +8,8 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { ProjectAccessComponent } from './project-access.component';
 
 describe('ProjectAccessComponent', () => {
@@ -16,6 +18,10 @@ describe('ProjectAccessComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+      ],
       declarations: [ProjectAccessComponent],
     })
       .compileComponents();
--- client/src/app/parts/project-access/project-access.component.ts
+++ client/src/app/parts/project-access/project-access.component.ts
@@ -6,7 +6,9 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DisplayOptions } from 'src/app/models/generic';
+import { ProjectAccess } from 'src/app/models/project';
 
 @Component({
   selector: 'sp-project-access',
@@ -14,10 +16,101 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-access.component.sass'],
 })
 export class ProjectAccessComponent implements OnInit {
+  /**
+   * Project access
+   */
+  access: ProjectAccess;
+
+  /**
+   * Render text?
+   */
+  asText = true;
+
+  /**
+   * Render as icon?
+   */
+  asIcon = false;
+
+  /**
+   * Render text as tooltip to icon?
+   */
+  withTooltip = false;
+
+  /**
+   * Text label.
+   */
+  label = '';
+
+  /**
+   * Tooltip.
+   */
+  tooltip = '';
+
+  /**
+   * Name of icon - based on value 'access'.
+   */
+  icon = '';
+
+  /**
+   * Name of class - based on value 'access'.
+   */
+  variant = '';
+
+  @Input()
+  set arg(val: ProjectAccess) {
+    if (!val) {
+      this.access = null;
+      return;
+    }
+
+    this.access = val;
+
+    switch (val) {
+      case ProjectAccess.PUBLIC:
+        this.label = 'PUBLIC';
+        this.icon = 'access_public';
+        this.variant = 'access-public';
+        this.tooltip = 'Project can be viewed by any user.';
+        break;
+
+      case ProjectAccess.VERIFIED:
+        this.label = 'VERIFIED';
+        this.icon = 'access_verified';
+        this.variant = 'access-verified';
+        this.tooltip = 'Project can be viewed by verified users only.';
+        break;
+
+      case ProjectAccess.SPECIFIC:
+        this.label = 'SPECIFIC';
+        this.icon = 'access_specific';
+        this.variant = 'access-specific';
+        this.tooltip = 'Project can be viewed by specific users only.';
+        break;
+
+      case ProjectAccess.ADMIN:
+        this.label = 'ADMINS';
+        this.icon = 'access_admin';
+        this.variant = 'access-admin';
+        this.tooltip = 'Project can be viewed by admins only.';
+        break;
+
+      default:
+    }
+  }
+
+  @Input()
+  set as(str: string) {
+    if (str) {
+      const tokens = str.split(/\s*,\s*/);
+
+      this.asText = tokens.includes(DisplayOptions.TEXT);
+      this.asIcon = tokens.includes(DisplayOptions.ICON);
+      this.withTooltip = tokens.includes(DisplayOptions.TOOLTIP);
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
