65686db0f503e83f6dadbd60210a96470a9a06b4
Sanjeev Premi <spremi@ymail.com>
2019-05-01 20:34:15 +0530

    feat: c: parts/project-health: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/project-health/project-health.component.spec.ts
+++ client/src/app/parts/project-health/project-health.component.spec.ts
@@ -8,6 +8,8 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { ProjectHealthComponent } from './project-health.component';
 
 describe('ProjectHealthComponent', () => {
@@ -16,6 +18,10 @@ describe('ProjectHealthComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+      ],
       declarations: [ProjectHealthComponent],
     })
       .compileComponents();
--- client/src/app/parts/project-health/project-health.component.ts
+++ client/src/app/parts/project-health/project-health.component.ts
@@ -6,7 +6,9 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DisplayOptions } from 'src/app/models/generic';
+import { ProjectHealth } from 'src/app/models/project';
 
 @Component({
   selector: 'sp-project-health',
@@ -14,10 +16,115 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-health.component.sass'],
 })
 export class ProjectHealthComponent implements OnInit {
+  /**
+   * Project health
+   */
+  health: ProjectHealth;
+
+  /**
+   * Render text?
+   */
+  asText = true;
+
+  /**
+   * Render as icon?
+   */
+  asIcon = false;
+
+  /**
+   * Render text as tooltip to icon?
+   */
+  withTooltip = false;
+
+  /**
+   * Text label.
+   */
+  label = '';
+
+  /**
+   * Tooltip.
+   */
+  tooltip = '';
+
+  /**
+   * Name of icon - based on value 'health'.
+   */
+  icon = '';
+
+  /**
+   * Style variant to use.
+   */
+  variant = '';
+
+  @Input()
+  set arg(val: ProjectHealth) {
+    if (!val) {
+      this.health = null;
+      return;
+    }
+
+    this.health = val;
+
+    switch (val) {
+      case ProjectHealth.LEVEL0:
+        this.label = 'LEVEL 0';
+        this.icon = 'health_l0';
+        this.variant = 'health-l0';
+        this.tooltip = 'None of 5 recent builds was successful.';
+        break;
+
+      case ProjectHealth.LEVEL1:
+        this.label = 'LEVEL 1';
+        this.icon = 'health_l1';
+        this.variant = 'health-l1';
+        this.tooltip = 'Only 1 of 5 recent builds was successful.';
+        break;
+
+      case ProjectHealth.LEVEL2:
+        this.label = 'LEVEL 2';
+        this.icon = 'health_l2';
+        this.variant = 'health-l2';
+        this.tooltip = '2 of 5 recent builds was successful.';
+        break;
+
+      case ProjectHealth.LEVEL3:
+        this.label = 'LEVEL 3';
+        this.icon = 'health_l3';
+        this.variant = 'health-l3';
+        this.tooltip = '3 of 5 recent builds was successful.';
+        break;
+
+      case ProjectHealth.LEVEL4:
+        this.label = 'LEVEL 4';
+        this.icon = 'health_l4';
+        this.variant = 'health-l4';
+        this.tooltip = '4 of 5 of recent builds were successful.';
+        break;
+
+      case ProjectHealth.LEVEL5:
+        this.label = 'LEVEL 5';
+        this.icon = 'health_l5';
+        this.variant = 'health-l5';
+        this.tooltip = 'All of 5 recent builds were successful.';
+        break;
+
+      default:
+    }
+  }
+
+  @Input()
+  set as(str: string) {
+    if (str) {
+      const tokens = str.split(/\s*,\s*/);
+
+      this.asText = tokens.includes(DisplayOptions.TEXT);
+      this.asIcon = tokens.includes(DisplayOptions.ICON);
+      this.withTooltip = tokens.includes(DisplayOptions.TOOLTIP);
+    }
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
