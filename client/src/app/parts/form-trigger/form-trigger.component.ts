3954943c0d546772135799017e403fe6788abb39
Sanjeev Premi <spremi@ymail.com>
2019-05-03 21:50:23 +0530

    feat: c: parts/form-trigger: Implement ControlValueAccessor interface
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/form-trigger/form-trigger.component.ts
+++ client/src/app/parts/form-trigger/form-trigger.component.ts
@@ -6,16 +6,23 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { forwardRef, Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Triggers } from 'src/app/models/triggers';
 
 @Component({
   selector: 'sp-form-trigger',
   templateUrl: './form-trigger.component.html',
   styleUrls: ['./form-trigger.component.sass'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormTriggerComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormTriggerComponent implements OnInit {
-
+export class FormTriggerComponent implements OnInit, ControlValueAccessor {
   /**
    * Form group for trigger.
    */
@@ -24,10 +31,55 @@ export class FormTriggerComponent implements OnInit {
   @Input() edit: boolean;
 
   constructor(private fb: FormBuilder) {
+  }
+
+  onTouched: () => void = () => { };
+
+  ngOnInit() {
     this.triggerForm = this.createForm();
   }
 
-  ngOnInit() {
+  /**
+   * Write new value.
+   * @param val Write new value
+   */
+  writeValue(val: Triggers): void {
+    if (val) {
+      this.triggerForm.setValue({
+        user: val.user,
+        hook: val.hook,
+        hookToken: val.hookAttrs.token,
+        schedule: val.schedule,
+        scheduleCron: val.schedAttrs.cron,
+        after: val.after,
+        afterProjects: val.afterAttrs.projects,
+        afterFailure: val.afterAttrs.onFailure,
+      },
+        {
+          emitEvent: false,
+        });
+    }
+  }
+
+  /**
+   * Registers callback function for value changes in the UI.
+   */
+  registerOnChange(fn: any): void {
+    this.triggerForm.valueChanges.subscribe(fn);
+  }
+
+  /**
+   * Registers callback function for 'blur' event.
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Called when the form is (enabled or) disabled.
+   */
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.triggerForm.disable() : this.triggerForm.enable();
   }
 
   /**
