25f01a42f743313bf869990355ae4078a950fc64
Sanjeev Premi <spremi@ymail.com>
2019-05-03 22:47:10 +0530

    feat: c: /project-info: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/routes/project-info/project-info.component.spec.ts
+++ client/src/app/routes/project-info/project-info.component.spec.ts
@@ -8,6 +8,21 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { InertDirective } from 'src/app/dictums/inert/inert.directive';
+import { MaterialModule } from 'src/app/modules/material/material.module';
+import { FormEmailComponent } from 'src/app/parts/form-email/form-email.component';
+import { FormProtocolsComponent } from 'src/app/parts/form-protocols/form-protocols.component';
+import { FormScmComponent } from 'src/app/parts/form-scm/form-scm.component';
+import { FormStepComponent } from 'src/app/parts/form-step/form-step.component';
+import { FormTriggerComponent } from 'src/app/parts/form-trigger/form-trigger.component';
+import { FormWorkspaceComponent } from 'src/app/parts/form-workspace/form-workspace.component';
+import { ProjectAccessComponent } from 'src/app/parts/project-access/project-access.component';
+import { ProjectHealthComponent } from 'src/app/parts/project-health/project-health.component';
+import { ProjectStateComponent } from 'src/app/parts/project-state/project-state.component';
+import { ProjectStatusComponent } from 'src/app/parts/project-status/project-status.component';
 import { ProjectInfoComponent } from './project-info.component';
 
 describe('ProjectInfoComponent', () => {
@@ -16,7 +31,27 @@ describe('ProjectInfoComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProjectInfoComponent],
+      imports: [
+        RouterTestingModule,
+        NoopAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+      ],
+      declarations: [
+        ProjectInfoComponent,
+        ProjectAccessComponent,
+        ProjectHealthComponent,
+        ProjectStateComponent,
+        ProjectStatusComponent,
+        FormEmailComponent,
+        FormProtocolsComponent,
+        FormScmComponent,
+        FormStepComponent,
+        FormTriggerComponent,
+        FormWorkspaceComponent,
+        InertDirective,
+      ],
     })
       .compileComponents();
   }));
--- client/src/app/routes/project-info/project-info.component.ts
+++ client/src/app/routes/project-info/project-info.component.ts
@@ -7,6 +7,14 @@
 //
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import {
+  initProject, Project, ProjectAccess, ProjectHealth, ProjectStatus
+} from 'src/app/models/project';
+import { RepoAccess, ScmTypes } from 'src/app/models/repo';
+
+import { cloneDeep } from 'lodash';
 
 @Component({
   selector: 'sp-project-info',
@@ -14,10 +22,137 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-info.component.sass'],
 })
 export class ProjectInfoComponent implements OnInit {
+  /**
+   * Dummy project info
+   */
+  public project: Project = {
+    id: '1',
+    name: 'Project 1',
+    desc: 'This is a dummy project.',
+    enabled: true,
+    status: ProjectStatus.NEVER,
+    health: ProjectHealth.LEVEL0,
+    build: {
+      latest: 'b00',
+      lastSuccess: 'Mar 10 2019 23:41:24',
+      lastFailure: 'Mar 10 2019 23:42:54',
+      lastDuration: 240,
+    },
+    repos: [
+      {
+        id: 'repo01',
+        repo: {
+          type: ScmTypes.GIT,
+          url: '',
+          label: '',
+          credId: 'c00',
+          refspec: 'refspec',
+          branches: [],
+          advanced: null,
+        },
+        status: {
+          access: RepoAccess.NOREQ,
+          error: '',
+        },
+      },
+    ],
+    perms: {
+      access: ProjectAccess.PUBLIC,
+      viewers: [],
+      builders: [],
+      admins: [],
+    },
+    triggers: {
+      user: true,
+      hook: true,
+      hookAttrs: {
+        token: 'secret_hook_token',
+      },
+      schedule: true,
+      schedAttrs: {
+        cron: '5 * * * *',
+      },
+      after: true,
+      afterAttrs: {
+        projects: ['proj1', 'proj2'],
+        onFailure: false,
+      },
+    },
+  };
 
-  constructor() { }
+  /**
+   * Form group for this component.
+   */
+  projectForm: FormGroup;
+
+  /**
+   * Can user edit the form?
+   */
+  canEdit = false;
+
+  /**
+   * Is user editing the form?
+   */
+  isEditing = false;
+
+  /**
+   * Flag: Add new project?
+   */
+  add = false;
+
+  constructor(private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
+    const data = this.route.snapshot.data;
+
+    if (data && data.hasOwnProperty('add') && data.add) {
+      this.add = true;
+      this.isEditing = true;
+
+      this.project = initProject();
+    }
+
+    this.projectForm = this.createForm();
   }
 
+  /**
+   * Discard changes.
+   */
+  onDiscard() {
+    this.isEditing = false;
+
+    this.projectForm.reset({
+      id: this.project.id,
+      name: this.project.name,
+      desc: this.project.desc,
+      enabled: this.project.enabled,
+      build: cloneDeep(this.project.build),
+      repos: cloneDeep(this.project.repos),
+      perms: cloneDeep(this.project.perms),
+      triggers: cloneDeep(this.project.triggers),
+    });
+
+    this.projectForm.markAsPristine();
+    this.projectForm.markAsUntouched();
+  }
+
+  /**
+   * Save changes.
+   */
+  onSave() {
+    this.isEditing = false;
+
+    console.log(this.projectForm.value);
+  }
+
+  /**
+   * Creates form group based on specified data
+   */
+  private createForm(): FormGroup {
+    return this.fb.group({
+      name: [this.project.name, Validators.required],
+      desc: [this.project.desc],
+      triggers: [this.project.triggers],
+    });
+  }
 }
