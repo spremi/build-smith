f1ad31af68b1bae3779fe2bbbc4cb00a59b5f20e
Sanjeev Premi <spremi@ymail.com>
2019-05-04 15:21:09 +0530

    feat: c: parts/form-email: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/form-email/form-email.component.spec.ts
+++ client/src/app/parts/form-email/form-email.component.spec.ts
@@ -8,6 +8,10 @@
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { InertDirective } from 'src/app/dictums/inert/inert.directive';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 import { FormEmailComponent } from './form-email.component';
 
 describe('FormEmailComponent', () => {
@@ -16,7 +20,13 @@ describe('FormEmailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [FormEmailComponent],
+      imports: [
+        NoopAnimationsModule,
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [FormEmailComponent, InertDirective],
     })
       .compileComponents();
   }));
--- client/src/app/parts/form-email/form-email.component.ts
+++ client/src/app/parts/form-email/form-email.component.ts
@@ -6,7 +6,8 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'sp-form-email',
@@ -14,10 +15,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-email.component.sass'],
 })
 export class FormEmailComponent implements OnInit {
+  /**
+   * Form group for email component.
+   */
+  emailForm: FormGroup;
 
-  constructor() { }
+  /**
+   * Show bcc?
+   */
+  showBcc = false;
 
-  ngOnInit() {
+  /**
+   * Show email header?
+   */
+  showHeader = false;
+
+  /**
+   * Show email footer?
+   */
+  showFooter = false;
+
+  @Input() edit: boolean;
+
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.emailForm = this.createForm();
+  }
+
+  /**
+   * Creates form group based on specified data.
+   */
+  private createForm(): FormGroup {
+    return this.fb.group({
+      to: ['', { updateOn: 'blur' }],
+      cc: ['', { updateOn: 'blur' }],
+      bcc: ['', { updateOn: 'blur' }],
+      subject: ['', { updateOn: 'blur' }],
+      body: ['', { updateOn: 'blur' }],
+      header: ['', { updateOn: 'blur' }],
+      footer: ['', { updateOn: 'blur' }],
+    });
+  }
 }
