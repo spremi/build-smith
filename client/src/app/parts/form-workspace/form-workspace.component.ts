d893b92968b379970b95436ec48dfa21b8399b5a
Sanjeev Premi <spremi@ymail.com>
2019-05-04 12:02:14 +0530

    feat: c: parts/form-workspace: Implement ControlValueAccessor interface
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/form-workspace/form-workspace.component.ts
+++ client/src/app/parts/form-workspace/form-workspace.component.ts
@@ -6,15 +6,23 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { forwardRef, Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Workspace } from 'src/app/models/workspace';
 
 @Component({
   selector: 'sp-form-workspace',
   templateUrl: './form-workspace.component.html',
   styleUrls: ['./form-workspace.component.sass'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormWorkspaceComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormWorkspaceComponent implements OnInit {
+export class FormWorkspaceComponent implements OnInit, ControlValueAccessor {
   /**
    * Form group for workspace.
    */
@@ -31,8 +39,51 @@ export class FormWorkspaceComponent implements OnInit {
   constructor(private fb: FormBuilder) {
   }
 
+  onTouched: () => void = () => { };
+
   ngOnInit() {
     this.workspaceForm = this.createForm();
+  }
+
+  /**
+   * Write new value.
+   * @param val New value
+   */
+  writeValue(val: Workspace): void {
+    if (val) {
+      this.workspaceForm.setValue({
+        workspaceDir: val.dir,
+        buildDir: val.buildDir,
+        recordDir: val.recordDir,
+        cleanBefore: val.cleanBefore,
+        cleanAfter: val.cleanAfter,
+        buildCount: val.buildCount === 0 ? '' : val.buildCount,
+      },
+        {
+          emitEvent: false,
+        });
+    }
+  }
+
+  /**
+   * Registers callback function for value changes in the UI.
+   */
+  registerOnChange(fn: any): void {
+    this.workspaceForm.valueChanges.subscribe(fn);
+  }
+
+  /**
+   * Registers callback function for 'blur' event.
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Called when the form is (enabled or) disabled.
+   */
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.workspaceForm.disable() : this.workspaceForm.enable();
   }
 
   /**
