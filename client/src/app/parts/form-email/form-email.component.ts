a7de1576ccfa288b81ff02b91b9093710f37fad2
Sanjeev Premi <spremi@ymail.com>
2019-05-04 15:30:34 +0530

    feat: c: parts/form-email: Implement ControlValueAccessor interface
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- client/src/app/parts/form-email/form-email.component.ts
+++ client/src/app/parts/form-email/form-email.component.ts
@@ -6,15 +6,23 @@
 // Available under terms of the BSD-3-Clause license.
 //
 
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { forwardRef, Component, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { EmailNotification } from 'src/app/models/notifications';
 
 @Component({
   selector: 'sp-form-email',
   templateUrl: './form-email.component.html',
   styleUrls: ['./form-email.component.sass'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FormEmailComponent),
+      multi: true,
+    },
+  ],
 })
-export class FormEmailComponent implements OnInit {
+export class FormEmailComponent implements OnInit, ControlValueAccessor {
   /**
    * Form group for email component.
    */
@@ -40,8 +48,56 @@ export class FormEmailComponent implements OnInit {
   constructor(private fb: FormBuilder) {
   }
 
+  onTouched: () => void = () => { };
+
   ngOnInit() {
     this.emailForm = this.createForm();
+  }
+
+  /**
+   * Write new value.
+   * @param val New value
+   */
+  writeValue(val: EmailNotification): void {
+    if (val) {
+      this.emailForm.setValue({
+        to: val.to,
+        cc: val.cc,
+        bcc: val.bcc,
+        subject: val.subject,
+        body: val.body,
+        header: val.header,
+        footer: val.footer,
+      },
+        {
+          emitEvent: true,
+        });
+
+      this.showBcc = !!val.bcc;
+      this.showHeader = !!val.header;
+      this.showFooter = !!val.footer;
+    }
+  }
+
+  /**
+   * Registers callback function for value changes in the UI.
+   */
+  registerOnChange(fn: any): void {
+    this.emailForm.valueChanges.subscribe(fn);
+  }
+
+  /**
+   * Registers callback function for 'blur' event.
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Called when the form is (enabled or) disabled.
+   */
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.emailForm.disable() : this.emailForm.enable();
   }
 
   /**
